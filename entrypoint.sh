dd0dc3699feea0b618f165f38255bfbad6b2d47d
artiga033 <artiga033@hotmail.com>
2024-05-15 11:40:55 +0800

    gracefully shutdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 set -m
+
+# gracefully shutdown
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM $(jobs -p)
+}
+trap _term SIGTERM
+
 # Xvfb for a virtual display
 Xvfb :0 -screen 0 1920x1080x24 -listen tcp -ac &
 # vnc server
